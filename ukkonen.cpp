88556064af8d42550105d8d9f6207a0e1eb477b3
rainbow <rain@rain.rain>
2014-06-01 18:22:03 +0400

    first files

---
+++ ukkonen.cpp
@@ -0,0 +1,31 @@
+
+//
+// g++ -std=c++11 -I /usr/local/include/boost-1_54/ ukkonen.cpp -o ukk
+//
+#include "ukkonen.hpp"
+
+
+
+int main (int argc, char *argv[]) {
+	std::string dat;
+	
+	
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
