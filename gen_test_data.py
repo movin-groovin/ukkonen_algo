2d30838e12f709f86d6af024c00a3a980d5bacf5
rainbow <rainbow@dhcppc1.one>
2015-02-22 00:36:35 +0000

    new files

--- gen_test_data.py
+++ gen_test_data.py
@@ -14,10 +14,18 @@ def main ():
 	number = 100
 	str_num = 28
 	
+	
+	if len(sys.argv) > 1 and sys.argv[1] == '-h':
+		print ("Usage: ./gen_test_data.py len_in_string*36 number_of_strings \n"\
+			   "Example: ./gen_test_data.py 10 10 - 3600 char length of data"\
+			   "Default: ./gen_test_data.py 28 100"
+	    )
+		return 0
+	
 	try:
 		if len(sys.argv) > 2:
-			number = int (sys.argv[1])
-			str_num = int (sys.argv[2])
+			number = int (sys.argv[2])
+			str_num = int (sys.argv[1])
 	except Exception as Exc:
 		print "An exception: ", Exc
 	
